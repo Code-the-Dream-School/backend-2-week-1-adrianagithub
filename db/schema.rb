e90710bf411018345e818e2081355de5db5196af
Adi <adi@Adis-MacBook-Air.local>
2020-09-18 22:23:31 -0700

    Updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200918225656) do
+ActiveRecord::Schema.define(version: 20200919050538) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "tittle"
+    t.text     "body"
+    t.string   "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "tittle"
