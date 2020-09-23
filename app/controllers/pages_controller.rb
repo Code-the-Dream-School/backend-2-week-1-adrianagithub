18246725bdccd79260f9e6907f0584dee08926df
Adi <adi@Adis-Air.attlocal.net>
2020-09-23 01:43:12 -0700

    New updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,28 @@ class PagesController < ApplicationController
     def index
       @pages = Page.all
     end
+    def show
+      @page = Page.find(params[:id])
+      #render text: @page.tittle#params[:id]
+    end
+    def new
+      @page = Page.new
+    end
+    def create
+      @page = Page.new(page_params)
+      @page.save
+      redirect_to @page
+    end
+    def edit
+      @page = Page.find(params[:id])
+    end
+    def update
+      @page = Page.find(params[:id])
+      @page.update(page_params)
+      redirect_to @page
+    end
+    private
+      def page_params
+        params.require(:page).permit(:tittle, :body, :slug)
+      end  
 end
